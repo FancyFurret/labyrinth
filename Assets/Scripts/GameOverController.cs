82d234091b29041615b5e10213615fe6fa9533df
osum4est <osumforrest@gmail.com>
2015-01-30 10:06:09 -0700

    added gameover

---
+++ Assets/Scripts/GameOverController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverController : MonoBehaviour {
+
+	public PlayerHealth playerHealth;
+	Animator anim;
+	bool gameOver;
+
+	void Awake () {
+		anim = GetComponent<Animator> ();
+		gameOver = false;
+	}
+
+	void OnGUI () {
+		if (playerHealth.health <= 0)
+			GameOver();
+
+		if (gameOver) {
+						if (Input.GetMouseButtonDown (0))
+								Application.LoadLevel ("MainMenu");
+				}
+	}
+
+	void GameOver()
+	{
+		anim.SetTrigger ("GameOver");
+		gameOver = true;
+		Screen.showCursor = false;
+	}
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public Slider slider;
+	public int health = 100;
+
+	public void Awake()
+	{
+		slider.maxValue = health;
+		slider.value = health;
+	}
+
+	public void TakeDamage(int amount)
+	{
+				health -= amount;
+				slider.value = health;
+		}
+}
