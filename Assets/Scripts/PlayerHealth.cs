51d9e54de0a3eb148fd3a1b9c9095188b6e0af14
osum4est <osumforrest@gmail.com>
2015-02-01 23:04:30 -0700

    gameover, health, flash working

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,8 +4,13 @@ using System.Collections;
 
 public class PlayerHealth : MonoBehaviour {
 
+    public Image damageImage;
+    public Color damageColor;
+    public float flashSpeed = 5f;
 	public Slider slider;
 	public int health = 100;
+
+    bool damaged;
 
 	public void Awake()
 	{
@@ -13,9 +18,20 @@ public class PlayerHealth : MonoBehaviour {
 		slider.value = health;
 	}
 
+    public void Update()
+    {
+        if (damaged)
+            damageImage.color = damageColor;
+        else
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+
+        damaged = false;
+    }
+
 	public void TakeDamage(int amount)
 	{
 				health -= amount;
 				slider.value = health;
+                damaged = true;
 		}
 }
